fa221d9bbbaac1d0a816a653b9f35bfcc8a9cf21
Nicole Golden <nicole.golden88@gmail.com>
2022-03-18 22:16:52 -0700

    add code
    add code

---
+++ README.md
@@ -0,0 +1,102 @@
+
+  <h1>Data Visualization: <br>What Are People in Russia Searching on Google?</h1>
+<p>Author: Nicole Golden<br> Date: March 18, 2022 </p>
+        
+<p class="lead">
+<br>
+On March 5, <span style="font-weight: 600">The Economist</span> publish an article titled "Russians are trying to flee Putin’s chaos" along with a plot below (see Figure 1). This plot was created using <span style="font-weight: 600">Google Trends</span> data. The graph sends an alarming message: there is no war inside Russia, but the people inside the country getting out. Even though President Putin is shutting down free media inside the country, some people are still getting the real news through other resources.   
+ <br><br>
+</p>
+       
+<center>            
+<img src="figure/economist_magazine.png">
+<h6>Figure 1 (Image Source: <a href="https://www.economist.com/graphic-detail/2022/03/04/russians-are-trying-to-flee-putins-chaos?utm_medium=social-media.content.np&utm_source=twitter&utm_campaign=editorial-social&utm_content=discovery.content" target="_blank">The Economist</a>) 
+</h6>
+</center>
+                
+<p class="lead">    
+Inspired by this article, I decided to do some data visualization by expanding the keywords searched on Google in Russia. That said, the searches are not only by Russian people but also by foreigners who reside in Russia. The original article includes only four keywords: flights, emigration, visa, political asylum (The data is from March 2021 to February 2022). In addition to these four keywords, I added extra words that may be of people's interest in Russia triggered by the war in Ukraine, and I grouped these words into six categories: <br>
+(1) War-related; <br>
+(2) Money-related; <br>
+(3) Politics-related; <br>
+(4) Immigration-related; <br>
+(5) Speech-related; <br>
+(6) Country/City-related.
+
+<br><br>
+                
+For each keyword, I also attached the Russian version alongside (correct me if the Russian words are wrong). The data is collected from <a href="https://trends.google.com/trends/?geo=US">Google Trends</a> and from period March 2021 to March 2022. At a glance, all the keywords spiked once the war broke out in Ukraine. Some of the results show abnormalities pre-war time; some already have high searches before the war. I will try to explain the plots in the rest of the article.     
+</p>
+
+<center>            
+<img src="figure/all_war.png">
+<h6>Figure 2 (Image Source: Nicole Golden. Data Source: 
+<a href="https://trends.google.com/trends/?geo=US" target="_blank">Google Trends</a>) 
+</h6>
+</center>
+            
+<p class="lead">    
+In Figure 2, people searched general terms related to war such as "war," "nuclear," and "soldiers." "Invasion" is a <a href="https://www.npr.org/2022/03/05/1084729579/russian-law-bans-journalists-from-calling-ukraine-conflict-a-war-or-an-invasion" target="_blank">banned</a> by Russian law. If journalists cannot use this term, Russian residents are definitely getting real news from other sources such as VPN, friends or relatives. <span style="font-weight: 600">The Guardian</span> reported that more than <a href="https://www.theguardian.com/world/2022/mar/18/ukraine-russia-families-divided-over-war" target="_blank">20 million</a> Ukrainians have family in Russia (This number may be conservative since this was estimated in <a href="https://rb.com.ua/blog/rossija-glazami-ukraincev/" target="_blank">a survey</a> in 2011). Term "Z" is a new term that gained fresh popularity out of this war. You can look at this <a href="https://en.wikipedia.org/wiki/Z_(military_symbol)" target="_blank">Wikipedia</a> page to get a general idea.   
+</p>
+
+<center>            
+<img src="figure/all_politics.png">
+<h6>Figure 3 (Image Source: Nicole Golden. Data Source: 
+<a href="https://trends.google.com/trends/?geo=US" target="_blank">Google Trends</a>) 
+</h6>
+</center>
+            
+<p class="lead">    
+Following the war, US and EU imposed severe economic <a href="https://www.bloomberg.com/news/articles/2022-02-28/sanctions-imposed-so-far-on-russia-from-the-u-s-eu-and-u-k" target="_blank">sanctions</a> against Russia (see Figure 3). People are very aware of it since it affects their daily lives. Putin's name was searched a lot since he decided the war. The search on "Nazi" also shoots up. This may be related to Putin's claim on Ukraine's "<a href="https://www.nbcnews.com/think/opinion/ukraine-has-nazi-problem-vladimir-putin-s-denazification-claim-war-ncna1290946" target="_blank">Nazi problem</a>" (which is a false claim, of course). People are also concerned to live under "Martial law," which will make normal people's life even harder (see <a href="https://en.wikipedia.org/wiki/Martial_law" target="_blank">Wikipedia</a> for its definition). Many western companies are leaving Russia, including beloved McDonald's. This <a href="https://www.youtube.com/watch?v=ckbfS99N6jY" target="_blank">video</a> shows the first launch of McDonald's in 1990. People queued hours outside in the cold winter just to take a bite of its juicy burger. After McDonald's also announced its departure, people flocked to restaurant to get a <a href="https://tatvabodhini.com/russians-grab-their-last-meal-at-mc-donalds-as-it-announces-shutdown/" target="_blank">last bite</a>.       
+</p>
+
+<center>
+<img src="figure/all_money.png">
+<h6>Figure 4 (Image Source: Nicole Golden. Data Source: 
+<a href="https://trends.google.com/trends/?geo=US" target="_blank">Google Trends</a>) 
+</h6>
+</center>
+            
+<p class="lead">  
+(Figure 4) There came the sanction. Then there came the money problem. Rouble value <a href="https://www.reuters.com/business/russian-rouble-tumbles-record-low-2022-02-28/" target="_blank">tumbled</a>; people go to <a href="https://www.reuters.com/world/europe/russians-queue-cash-west-targets-banks-over-ukraine-2022-02-27/" target="_blank">banks</a> wishing to secure some money/cash, or exchange some dollars to secure its values. Then there came the troubled <a href="https://www.reuters.com/world/europe/russian-inflation-accelerates-125-highest-since-2015-2022-03-16/" target="_blank">inflation</a>, which is at a 12.5% rate of increase as of March 16.   
+</p>
+
+<center>         
+<img src="figure/all_immigration.png">
+<h6>Figure 5 (Image Source: Nicole Golden. Data Source: 
+                <a href="https://trends.google.com/trends/?geo=US" target="_blank">Google Trends</a>) 
+</h6>
+</center>
+
+<p class="lead">  
+(Figure 5) Then there is the exodus: as many as <a href="https://www.bbc.com/news/world-europe-60697763" target="_blank">200,000 Russians</a> are leaving the country. Many young people inside the country are <a href="https://qz.com/2135871/do-russians-support-war-with-ukraine/" target="_blank">against the war</a>. They are worried about the future of their county and of themselves. They decided to leave for a foreign country, despite not knowing anyone or not having a job. 
+</p>
+            
+<center>            
+<img src="figure/all_speech.png">
+<h6>Figure 6 (Image Source: Nicole Golden. Data Source: 
+                <a href="https://trends.google.com/trends/?geo=US" target="_blank">Google Trends</a>) 
+</h6>
+</center>
+            
+<p class="lead"> 
+(Figure 6) The Russian government is fighting a war that people see differently because the government is shutting down social media - Facebook, Twitter, one by one. People who have means know it is "propaganda" or "censor(ed)." The term "liar" has an interesting pattern: before the war broke out, it was already a popular term (though it has its ups and downs); then, it peaked when the war started. It is not clear to me which side people are accusing.  
+</p>
+            
+<center>            
+<img src="figure/all_countrycity.png">
+<h6>Figure 7 (Image Source: Nicole Golden. Data Source: 
+                <a href="https://trends.google.com/trends/?geo=US" target="_blank">Google Trends</a>) 
+</h6>
+</center>
+
+<p class="lead"> 
+                 (Figure 7) Naturally, "Russia" and "Ukraine" are searched more frequently after the war. As Russia's ally, "China" also appeared more times in the searches. The other cities, "Belgrade" and "Yerevan," are two popular destinations that Russians are fleeing to. 
+</p>
+               
+<p class="lead"> 
+At the end of the article, I'd like to share a website <a href="https://papapover.com/en/" target="_blank">Dad, believe</a> made by a Ukrainian man <a href="https://www.nytimes.com/2022/03/06/world/europe/ukraine-russia-families.html" target="_blank">Misha Katsurin</a>. He created this website because of similar experiences of other Ukrainians: their family in Russia <a href="https://www.theguardian.com/world/2022/mar/18/ukraine-russia-families-divided-over-war" target="_blank">don't believe</a> there is such a war (since the war-related news reported in Russia is not truthful). I just hope Ukrainians have their peace soon. 
+</p>
+
+<br><br><br><br>
+
---
+++ RussianWord.R
@@ -0,0 +1,387 @@
+
+# set working directory 
+setwd("/Users/nicolegolden/Documents/1. Data Science/*GitHub/Russian_project/")
+
+# load packages 
+library(data.table)
+library(dplyr)
+library(ggplot2)
+
+
+############################################
+###  1. Google Search Trends: 22 Weeks   ###
+############################################
+
+# Time: 10/17/21- 3/13/22 (22 weeks)
+
+# load data 
+keywords <- fread("data/keywords_2022.csv")
+
+
+#----------------------1. war-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = War, 
+                colour = "War (война)")) + 
+  geom_line(aes(y = Nuclear, 
+                colour = "Nuclear (ядерный)")) + 
+  geom_line(aes(y = Z, 
+                colour = "Z (Z)")) + 
+  geom_line(aes(y = Invasion, 
+                colour = "Invasion (вторжение)")) + 
+  geom_line(aes(y = Soldiers, 
+                colour = "Soldiers (солдаты)")) +
+  geom_line(aes(y = Sons, 
+                colour = "Sons (сыновья)")) +
+  labs(colour="War-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+  # Remove panel border
+  panel.border = element_blank(),  
+  # Remove panel grid lines
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  # Remove panel background
+  panel.background = element_blank(),
+  # Add axis line
+  axis.line = element_line(colour = "black")
+)
+
+#----------------------2. Money-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Inflation, 
+                colour = "Inflation (инфляция)")) + 
+  geom_line(aes(y = Bank, 
+                colour = "Bank (банк)")) + 
+  geom_line(aes(y = Money, 
+                colour = "Money (Деньги)")) + 
+  geom_line(aes(y = ExchangeRate, 
+                colour = "Exchange Rate (обменный курс)")) +
+  geom_line(aes(y = Rouble, 
+                colour = "Rouble (рубль)")) +
+  labs(colour="Money-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+#----------------------3. Politics-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Putin, 
+                colour = "Putin (Путин)")) + 
+  geom_line(aes(y = Nazi, 
+                colour = "Nazi (нацист)")) + 
+  geom_line(aes(y = MartialLaw, 
+                colour = "Martial Law (военное положение)")) + 
+  geom_line(aes(y = Sanction, 
+                colour = "Sanction (санкция)")) +
+  geom_line(aes(y = McDonalds, 
+                colour = "McDonalds (Макдоналдс)")) +
+  labs(colour="Politics-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+#----------------------4. Immigration-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Flights, 
+                colour = "Flights (полеты)")) + 
+  geom_line(aes(y = Visa, 
+                colour = "Visa (виза)")) + 
+  geom_line(aes(y = Immigrate, 
+                colour = "Immigrate (иммигрировать)")) + 
+  geom_line(aes(y = Emmigration, 
+                colour = "Emmigration (эмиграция)")) +
+  geom_line(aes(y = Gas, 
+                colour = "Gas (газ)")) +
+  geom_line(aes(y = PoliticalAsylum, 
+                colour = "Political Asylum (политическое убежище)")) +
+  labs(colour="Immigration-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+#----------------------5. Speech-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = News, 
+                colour = "News (Новости)")) + 
+  geom_line(aes(y = Propaganda, 
+                colour = "Propaganda (пропаганда)")) + 
+  geom_line(aes(y = Censor, 
+                colour = "Censor (цензор)")) + 
+  geom_line(aes(y = Liar, 
+                colour = "Liar (лжец)")) +
+  geom_line(aes(y = Facebook, 
+                colour = "Facebook (Facebook)")) +
+  geom_line(aes(y = Twitter, 
+                colour = "Twitter (Twitter)")) +
+  labs(colour="Speech-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+#----------------------6. Country/City-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Ukraine, 
+                colour = "Ukraine (Украина)")) + 
+  geom_line(aes(y = Russia, 
+                colour = "Russia (Россия)")) + 
+  geom_line(aes(y = China, 
+                colour = "China (Китай)")) + 
+  geom_line(aes(y = Belgrade, 
+                colour = "Belgrade (Белград)")) +
+  geom_line(aes(y = Yerevan, 
+                colour = "Yerevan (Ереван)")) +
+  labs(colour="Country/City-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+############################################
+###  2. Google Search Trends: 52 Weeks   ###
+############################################
+
+# Time: 3/21/21- 3/13/22 (52 weeks)
+
+# load data 
+keywords <- fread("data/keywords_all.csv")
+
+
+#----------------------1. war-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = War, 
+                colour = "War (война)")) + 
+  geom_line(aes(y = Nuclear, 
+                colour = "Nuclear (ядерный)")) + 
+  geom_line(aes(y = Z, 
+                colour = "Z (Z)")) + 
+  geom_line(aes(y = Invasion, 
+                colour = "Invasion (вторжение)")) + 
+  geom_line(aes(y = Soldiers, 
+                colour = "Soldiers (солдаты)")) +
+  labs(colour="War-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+#----------------------2. Money-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Inflation, 
+                colour = "Inflation (инфляция)")) + 
+  geom_line(aes(y = Bank, 
+                colour = "Bank (банк)")) + 
+  geom_line(aes(y = Money, 
+                colour = "Money (Деньги)")) + 
+  geom_line(aes(y = ExchangeRate, 
+                colour = "Exchange Rate (обменный курс)")) +
+  geom_line(aes(y = Rouble, 
+                colour = "Rouble (рубль)")) +
+  labs(colour="Money-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+#----------------------3. Politics-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Putin, 
+                colour = "Putin (Путин)")) + 
+  geom_line(aes(y = Nazi, 
+                colour = "Nazi (нацист)")) + 
+  geom_line(aes(y = MartialLaw, 
+                colour = "Martial Law (военное положение)")) + 
+  geom_line(aes(y = Sanction, 
+                colour = "Sanction (санкция)")) +
+  geom_line(aes(y = McDonalds, 
+                colour = "McDonalds (Макдоналдс)")) +
+  labs(colour="Politics-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+#----------------------4. Immigration-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Flights, 
+                colour = "Flights (полеты)")) + 
+  geom_line(aes(y = Visa, 
+                colour = "Visa (виза)")) + 
+  geom_line(aes(y = Immigrate, 
+                colour = "Immigrate (иммигрировать)")) + 
+  geom_line(aes(y = Emmigration, 
+                colour = "Emmigration (эмиграция)")) +
+  geom_line(aes(y = Gas, 
+                colour = "Gas (газ)")) +
+  geom_line(aes(y = PoliticalAsylum, 
+                colour = "Political Asylum (политическое убежище)")) +
+  labs(colour="Immigration-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+#----------------------5. Speech-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = News, 
+                colour = "News (Новости)")) + 
+  geom_line(aes(y = Propaganda, 
+                colour = "Propaganda (пропаганда)")) + 
+  geom_line(aes(y = Censor, 
+                colour = "Censor (цензор)")) + 
+  geom_line(aes(y = Liar, 
+                colour = "Liar (лжец)")) +
+  geom_line(aes(y = Facebook, 
+                colour = "Facebook (Facebook)")) +
+  geom_line(aes(y = Twitter, 
+                colour = "Twitter (Twitter)")) +
+  labs(colour="Speech-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+#----------------------6. Country/City-related keyword search
+ggplot(keywords, 
+       aes(week_number)) + 
+  geom_line(aes(y = Ukraine, 
+                colour = "Ukraine (Украина)")) + 
+  geom_line(aes(y = Russia, 
+                colour = "Russia (Россия)")) + 
+  geom_line(aes(y = China, 
+                colour = "China (Китай)")) + 
+  geom_line(aes(y = Belgrade, 
+                colour = "Belgrade (Белград)")) +
+  geom_line(aes(y = Yerevan, 
+                colour = "Yerevan (Ереван)")) +
+  labs(colour="Country/City-Related Keywords") + 
+  xlab("Week") + 
+  ylab("Google Search Trends") + 
+  theme(
+    # Remove panel border
+    panel.border = element_blank(),  
+    # Remove panel grid lines
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    # Remove panel background
+    panel.background = element_blank(),
+    # Add axis line
+    axis.line = element_line(colour = "black")
+  )
+
+
+
+
+
